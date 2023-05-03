7578381444ff2d82a2cdd6cbf6f30c9f752019c6
Henry-Liu0719 <hungminliu@gmail.com>
2023-05-03 09:08:54 +0800

    更改words陣列產生的方法，改'\n'切割，產製表格將依照案名排列

--- handleIndex.js
+++ handleIndex.js
@@ -1,83 +1,64 @@
-document.querySelector('.btn-primary').addEventListener('click',function(){
-  window.scrollTo({
-  top: 0,
-  left: 0,
-  behavior: 'smooth'
-});
-  var innerHTML=''
-  var isEmpty=false;
-  document.querySelector('.ifInputEmpty').classList.add('d-none');
-  if(document.querySelector('#usernameInput').value==''){
-    innerHTML+='<label for="usernameInput">未輸入申請人名稱(點擊前往)</label><br>'
-    isEmpty=true;
-  }
-  if(document.querySelector('#firstProjectName').value==''){
-    innerHTML+='<label for="firstProjectName">未輸入第一個案名(點擊前往)</label><br>'
-    isEmpty=true;
-  }
-  if(document.querySelector('#content').value==''){
-    innerHTML+='<label for="content">未輸入記事本文字(點擊前往)</label><br>'
-    isEmpty=true;
-  }
-  if(isEmpty){
-    document.querySelector('.ifInputEmpty').classList.remove('d-none');
-    document.querySelector('.ifInputEmpty').innerHTML=innerHTML;
-    
-  }
-})
+// onClick();
+function pageBehaviors(){
+  document.querySelector('.btn-primary').addEventListener('click',function(){
+    window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+    var innerHTML=''
+    var isEmpty=false;
+    document.querySelector('.ifInputEmpty').classList.add('d-none');
+    if(document.querySelector('#usernameInput').value==''){
+      innerHTML+='<label for="usernameInput">未輸入申請人名稱(點擊前往)</label><br>'
+      isEmpty=true;
+    }
+    // if(document.querySelector('#firstProjectName').value==''){
+    //   innerHTML+='<label for="firstProjectName">未輸入第一個案名(點擊前往)</label><br>'
+    //   isEmpty=true;
+    // }
+    if(document.querySelector('#content').value==''){
+      innerHTML+='<label for="content">未輸入記事本文字(點擊前往)</label><br>'
+      isEmpty=true;
+    }
+    if(isEmpty){
+      document.querySelector('.ifInputEmpty').classList.remove('d-none');
+      document.querySelector('.ifInputEmpty').innerHTML=innerHTML;
+      
+    }
+  })
+}
+pageBehaviors();
 function onClick(){
   let username = document.querySelector('#usernameInput').value;
   username = username.trim();
-  let str = document.querySelector('textarea').value;
+  let str = document.querySelector('textarea#content').value;
   let segments=[];
   str = str.replace('"', '\"');
-  // str = str.replace(/\u3000/g, '@');
-  // str = str.replace('	', '**');
-  // str = str.replace('	', '@@');
-  // str = str.replace(' ', '**');
-  // str=deleteChars(str,'　');
-  // var fullSpace = str[10];
-  // str = str.replace(fullSpace, '@@');
-//   var charCode = str.charCodeAt(10); // 獲取字符的 Unicode 編碼
-//   var asciiCode = charCode.toString(10); // 轉換成十進制的 ASCII 編碼
-  
-//   for (var i = 0; i <20; i++) {
-//   var charCode = str.charCodeAt(i); // 獲取字符的 Unicode 編碼
-//   var asciiCode = charCode.toString(10); // 轉換成十進制的 ASCII 編碼
-//   if(asciiCode=='10'){
-//     str[i]='@'
-//   }
-//   console.log(str[i] + " 的 ASCII 編碼是：" + asciiCode);
-// }
-
-  // str = deleteHeaderRow(str);
-  // str = encodeURIComponent(str);
   str=deleteChars(str,',');
   var month = getMonth(str);
+  str=deleteHeaderRow(str);
   // console.log(month)
-  while(str=cutByDate(str)){
+  while(str=cutByItemNum(str)){
     segments.push(getSegment(str));
   }
-  // str=cutByDate(str);
+  // str=cutByItemNum(str);
   var words=[]
   segments.forEach(element => {
-    words.push(convertSentenceToWords(element)); 
+    if(element.includes('/')){
+      words.push(convertSentenceToWords(element)); 
+    }
   });
+  sortWords(words);
+  console.log("words");
   console.log(words);
   var results=[];
   results=generateResult(words,username,month);
   // console.log(results[0]);
+  // results=sortArray(results);
+  // console.log(results);
   document.querySelector('tbody').innerHTML=renderTd(results);
 }
-// function onClick(){
-//   let username = document.querySelector('#usernameInput').value;
-//   let firstProjectName = document.querySelector('#firstProjectName').value;
-//   let str = document.querySelector('textarea').value;
-//   let segments=[];
-//   str=deleteChars(str,',');
-//   var month = getMonth(str);
-//   cutByNum(str);
-// }
 function deleteChars(str,charToRemove){
   const regex = new RegExp(charToRemove, "g"); 
   return str.replace(regex,"")
@@ -90,13 +71,12 @@ function getMonth(str){
   return str.trim()
   
 }
-function cutByDate(str){
-var startIndex = str.search('/');
+function cutByItemNum(str){
+var startIndex = str.search('\n');
 if(startIndex==-1)
   return false;
 str = str.substring(startIndex);
 startIndex = str.search(' ');
-// //console.log(`剩餘的句子:${str}`); // 輸出：加油 1,931 1,839 92 18,069
 return str = str.substring(startIndex);
 }
 function deleteHeaderRow(str){
@@ -104,7 +84,6 @@ function deleteHeaderRow(str){
   return str.substring(startIndex);
 }
 function cutByNum(str){
-  // //console.log(`cutByNum:${str}`);
   let i=1;
   while(str.search(` ${i} `)!=-1){
     if(str.search(` ${i} -`))
@@ -112,70 +91,63 @@ function cutByNum(str){
       var startIndex = str.search(` ${i} `);
     if(str.search(` ${i+1} -`))
       var startIndex = str.search(` ${i} `);
-      //卡關，startIndex= 1 ，!= 1 -
-      //endIndex= 2 ,!= 2 -
     
   }
-  // if(startIndex==-1)
-  //   return false;
-  // str = str.substring(startIndex);
-  // startIndex = str.search(' ');
-  // //console.log(`剩餘的句子:${str}`); // 輸出：加油 1,931 1,839 92 18,069
-  // return str = str.substring(startIndex);
 
 }
 function getSegment(str){
-  var endIndex = str.search('/');
+  var endIndex = str.search('\n');
   var segment = endIndex==-1?str.substring(0):str.substring(0,endIndex);
-  // //console.log(`截到的句子:${segment}`); // 輸出：加油 1,931 1,839 92 18,069
   return segment;
 }
 function convertSentenceToWords(str){
   var words=[];
-  for(let i=0;i<10;i++){
-  }
   do{
     var startIndex=str.search(' ');
     str=str.substring(startIndex+1);
-    // //console.log(str);
     var endIndex=str.search(' ');
     word=str.substring(0,endIndex);
     words.push(word);
   }while(str.search(' ')!=-1)
   words = words.filter((item)=>item!='')
+  // console.log(words);
   return words;
 }
+function sortWords(words){
+  // for(let i =0;i<words.length;i++){
+  //   words[i].push(i);
+  // }
+  words.sort((a, b) => {
+  if (a[0] < b[0]) {
+    return 1;
+  }
+  if (a[0] > b[0]) {
+    return -1;
+  }
+  return 0;
+});
+}
 function generateResult(array,username,month){
-    let firstProjectName = document.querySelector('#firstProjectName').value;
-    firstProjectName =firstProjectName.trim();
-    var items=[];
+    // let firstProjectName = document.querySelector('#firstProjectName').value;
+    // firstProjectName =firstProjectName.trim();
     var results=[];
     for(let i=0;i<array.length;i++){
-      console.log(array[i].includes('-'))
-      if(i==0){
-        if(array[i].includes('-')){
-
-          var money_full=`${array[i][1]}`;
-          var str_full=`${username}_${month}月份零用金_${firstProjectName}_${array[i][0]}`
-          var money_five=``;
-          var str_five=`${username}_5%_${firstProjectName}_${array[i][0]}`
-        }else{
-          var money_full=`${array[i][2]}`;
-          var str_full=`${username}_${month}月份零用金_${firstProjectName}_${array[i][0]}`
-          var money_five=`${array[i][3]}`;
-          var str_five=`${username}_5%_${firstProjectName}_${array[i][0]}`
-        }
-      }else if(array[i].includes('-')){
-        var money_full=`${array[i][1]}`;
+      var items=[];
+      // console.log(array[i].includes('-'))
+      if(array[i].includes('-')){
+        var money_full=`${array[i][3]}`;
         // console.log(money_full);
-        var str_full=`${username}_${month}月份零用金_${array[i-1][6]}_${array[i][0]}`
+        var str_full=`${username}_${month}月份零用金_${array[i][0]}_${array[i][2]}`
         var money_five=`無`;
-        var str_five=`${username}_5%_${array[i-1][6]}_${array[i][0]}`
+        var str_five=`${username}_5%_${array[i][0]}_${array[i][2]}`
+        // 申請人名稱_X月份零用金_案名_項目說明	
+        // 申請人名稱_5%_案名_項目說明
+
       }else{
-        var money_full=`${array[i][2]}`;
-        var str_full=`${username}_${month}月份零用金_${array[i-1][6]}_${array[i][0]}`
-        var money_five=`${array[i][3]}`;
-        var str_five=`${username}_5%_${array[i-1][6]}_${array[i][0]}`
+        var money_full=`${array[i][4]}`;
+        var str_full=`${username}_${month}月份零用金_${array[i][0]}_${array[i][2]}`
+        var money_five=`${array[i][5]}`;
+        var str_five=`${username}_5%_${array[i][0]}_${array[i][2]}`
       }
       items.push(money_full);
       items.push(str_full);
@@ -187,15 +159,19 @@ function generateResult(array,username,month){
     //console.log(results[0]);
     return results;
 }
+function sortArray(array){
+
+}
 function renderTd(array){
+  // console.log(array);
   var innerHTML=''
-  for(let i =0;i<array[0].length;i+=4){
+  for(let i =0;i<array.length;i++){
     innerHTML+=`
         <tr>
-          <td>${array[0][i]}</td>
-          <td>${array[0][i+2]}</td>
-          <td>${array[0][i+1]}</td>
-          <td>${array[0][i+3]}</td>
+          <td>${array[i][0]}</td>
+          <td>${array[i][2]}</td>
+          <td>${array[i][1]}</td>
+          <td>${array[i][3]}</td>
         </tr>
     `
   }
